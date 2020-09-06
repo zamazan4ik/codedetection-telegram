186d08bf1a6fa9654554494d2cc9447ec710c811
Alexander Zaitsev <zamazan4ik@tut.by>
2020-09-06 03:42:24 +0300

    feat: implement an automatic bot reponses removal
    - now bot handles user editions. If user edited correctly his message - bot removes its notification. Otherwise old notification is removed and a new one is sent
    
    Tested:
    - Local tests

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod commands;
 
 use teloxide::{prelude::*, utils::command::BotCommand};
 
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
 #[tokio::main]
 async fn main() {
     run().await;
@@ -14,38 +17,140 @@ async fn run() {
 
     let bot = Bot::from_env();
 
+    let bot_responses_to_messages = Arc::new(Mutex::new(HashMap::<i32, i32>::new()));
+    let bot_responses_to_edited_messages = bot_responses_to_messages.clone();
+
+
     Dispatcher::new(bot)
         .messages_handler(|rx: DispatcherHandlerRx<Message>| {
-            rx.for_each(|message| async move {
-                let message_text = match message.update.text() {
-                    Some(x) => x,
-                    None => return,
-                };
+            rx.for_each(move |message| {
+                let bot_responses_to_messages = bot_responses_to_messages.clone();
 
-                // Handle commands
-                match commands::Command::parse(message_text, "CodeDetectorBot") {
-                    Ok(command) => {
-                        commands::command_answer(&message, command).await.log_on_error().await;
+                async move {
+                    let message_text = match message.update.text() {
+                        Some(x) => x,
+                        None => return,
+                    };
+
+                    // Handle commands
+                    match commands::Command::parse(message_text, "CodeDetectorBot") {
+                        Ok(command) => {
+                            commands::command_answer(&message, command).await.log_on_error().await;
+                            return;
+                        }
+                        Err(_) => (),
+                    };
+
+                    // If message is formatted - just ignore it
+                    if detection::maybe_formatted(message.update.entities()) {
                         return;
                     }
-                    Err(_) => (),
-                };
 
-                // Handle code formatting
-                if detection::maybe_formatted(message.update.entities()) {
-                    return;
-                }
-
-                if detection::is_code_detected(message_text) {
-                    static FORMAT_TEXT: &str = "Оберните код в теги: 3 символа ` до и после кода \
+                    if detection::is_code_detected(message_text) {
+                        static FORMAT_TEXT: &str = "Оберните код в теги: 3 символа ` до и после кода \
                         (в случае одиночной конструкции достаточно 1 ` с обеих сторон). Спасибо!";
 
-                    message
-                        .reply_to(FORMAT_TEXT)
-                        .send()
-                        .await
-                        .log_on_error()
-                        .await;
+                        let response_message = message
+                            .reply_to(FORMAT_TEXT)
+                            .send()
+                            .await;
+
+                        match response_message {
+                            Ok(extracted_response_message) =>
+                                {
+                                    bot_responses_to_messages
+                                        .lock()
+                                        .unwrap()
+                                        .insert(message.update.id, extracted_response_message.id);
+                                }
+                            Err(_) => { response_message.log_on_error().await; }
+                        };
+                    }
+                }
+            })
+        })
+        .edited_messages_handler(|rx: DispatcherHandlerRx<Message>| {
+            rx.for_each(move |message| {
+                let bot_responses_to_messages = bot_responses_to_edited_messages.clone();
+                async move {
+                    let message_text = match message.update.text() {
+                        Some(x) => x,
+                        None => return,
+                    };
+
+                    // Handle code formatting
+                    if detection::maybe_formatted(message.update.entities()) {
+                        let maybe_bot_answer_id =
+                            bot_responses_to_messages
+                                .lock()
+                                .unwrap()
+                                .get(&message.update.id)
+                                .cloned();
+
+                        match maybe_bot_answer_id {
+                            Some(response) => {
+                                // Clear all related to the message bot responses
+                                message.bot
+                                    .delete_message(message.chat_id(), response)
+                                    .send()
+                                    .await
+                                    .log_on_error()
+                                    .await;
+
+                                bot_responses_to_messages
+                                    .lock()
+                                    .unwrap()
+                                    .remove(&message.update.id);
+
+                                return;
+                            }
+                            None => { return; }
+                        }
+                    } else if detection::is_code_detected(message_text) {
+                        // Delete old notification, then create new notification
+
+                        let old_notification = bot_responses_to_messages
+                            .lock()
+                            .unwrap()
+                            .get(&message.update.id)
+                            .cloned();
+
+                        match old_notification {
+                            Some(old_id) => {
+                                message.bot
+                                    .delete_message(message.chat_id(), old_id)
+                                    .send()
+                                    .await
+                                    .log_on_error()
+                                    .await;
+
+                                bot_responses_to_messages
+                                    .lock()
+                                    .unwrap()
+                                    .remove(&message.update.id);
+                            }
+                            None => ()
+                        }
+
+                        static FORMAT_TEXT: &str = "Всё ещё неправильно :( Оберните код в теги: 3 символа ` до и после кода \
+                        (в случае одиночной конструкции достаточно 1 ` с обеих сторон). Спасибо!";
+
+                        let response_message = message
+                            .reply_to(FORMAT_TEXT)
+                            .send()
+                            .await;
+
+                        match response_message {
+                            Ok(extracted_response_message) =>
+                                {
+                                    bot_responses_to_messages
+                                        .lock()
+                                        .unwrap()
+                                        .insert(message.update.id, extracted_response_message.id);
+                                }
+                            Err(_) => { response_message.log_on_error().await; }
+                        };
+                    }
                 }
             })
         })
